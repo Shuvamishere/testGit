67c891777ae2724f63fb4bd868d0b9ab83d4f7fa
Shuvamishere <ssamadder@ch.iitr.ac.in>
2020-03-30 03:18:43 +0530

    Don't know what I am doing

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+age = input("Please specify your age: ")
+print("Hello " + name + " of " + age +" year old.") 
+def succesful_commit():
+    print(name + ", the commit is succesful.")
+
+succesful_commit()
